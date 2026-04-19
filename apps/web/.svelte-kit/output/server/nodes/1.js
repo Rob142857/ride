be8c28d0f4b3f70571c788d9e20aa1aa7a665dae
Rob Evans <rme142857@gmail.com>
2026-04-19 12:58:33 +1000

    fix: deploy from public/, untrack .svelte-kit, update .gitignore

--- apps/web/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.BaCMUgAi.js","_app/immutable/chunks/BP4nCMQk.js","_app/immutable/chunks/Bp_-EhYr.js","_app/immutable/chunks/DILfUbMC.js","_app/immutable/chunks/BeNdFXIq.js","_app/immutable/chunks/D1gtkjhw.js"];
-export const stylesheets = [];
-export const fonts = [];
